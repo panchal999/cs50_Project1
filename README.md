af125a2c41734557e5a5c1df09662242ce5180bf
panchal999 <parth.np5@gmail.com>
2020-04-30 20:25:55 +0530

    updated Readme file

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+
+## Flask App file : application.py
+
+
+#Folder/Files Info:
+
+-classes: Folder contains python files for import 'book.csv' to db and to test goodreads api call (key info is given below).
+-templates: HTML web pages 
+-static: CSS/Images 
+-tables.sql : create queries of tables 
+
+
+
+#Goodreads API:
+key: 9GUF2T1jJgHkJKcFYsnkg
+
+
+
+
+
+
+
+
+
+
+
+
--- classes/import.py
+++ classes/import.py
@@ -11,9 +11,13 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 def main():
+
+	"""Add User Query """
 	# dateTimeObj = datetime.now()
 	# timestampStr = dateTimeObj.strftime("%d-%b-%Y %H:%M:%S.%f")
 	# db.execute("INSERT INTO users (name,password,email,created_on) VALUES (:name,:password,:email,:created_on)",{"name": 'abc',"password":'123',"email":'abc@gmail.com',"created_on": timestampStr })
+	
+	"""Add dummy Book Reviews """
 	db.execute("INSERT INTO book_review (user_id,book_id,rating,review) VALUES (:user_id,:book_id,:rating,:review)",{"user_id": 1,"book_id":2934 ,"rating": 4,"review": "1-Must Read, Amazing book!!" })
 	db.execute("INSERT INTO book_review (user_id,book_id,rating,review) VALUES (:user_id,:book_id,:rating,:review)",{"user_id": 2,"book_id":2934 ,"rating": 5,"review": "2-I love this book, enjoy present moment" })
 	db.execute("INSERT INTO book_review (user_id,book_id,rating,review) VALUES (:user_id,:book_id,:rating,:review)",{"user_id": 1,"book_id":4199 ,"rating": 5,"review": "1-BestSeller,Great Explanation!! " })
@@ -21,6 +25,8 @@ def main():
 	db.execute("INSERT INTO book_review (user_id,book_id,rating,review) VALUES (:user_id,:book_id,:rating,:review)",{"user_id": 1,"book_id":4 ,"rating": 4,"review": "Dedicated to future generation, Robots, Also has Movie" })
 	
 	db.commit()
+
+""" Add csv file data of book to database """
 # f = open("/home/parth/Documents/cs50/project1/books.csv")
 # reader = csv.reader(f)
 
